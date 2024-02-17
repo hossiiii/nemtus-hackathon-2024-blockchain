fd29d1c7a0df8e5efac43875c875ad0fecac031d
hossiiii <hossiiii0117@gmail.com>
2024-02-17 12:23:47 +0900

    logを入れる

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -21,7 +21,9 @@ export const ProductList = () => {
   const [productList, setProductList] = useState<ProductInfo[] | null>(null); //productList
 
   useEffect(() => {
+    console.log('ProductList useEffect')
     if (momijiBlockChain) {
+      console.log('ProductList useEffect momijiBlockChain')
       const momijiSellerPublicKey = localStorage.getItem(symbolSellerAccountMetaDataKey)
       if (!momijiSellerPublicKey) {
         setSnackbarSeverity('error');
